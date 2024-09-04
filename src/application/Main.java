715fd102e13fca672d5c79248f6e2d409d48e233
Daiane Farias <daiane.farias.contractor@gmail.com>
2024-09-04 13:35:51 +0100

    Initializing action as parameter

--- src/application/Main.java
+++ src/application/Main.java
@@ -22,7 +22,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
+		    mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
